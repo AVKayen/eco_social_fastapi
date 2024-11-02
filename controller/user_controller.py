65eef55e8c2291b072c7e514f5035e0d0205095a
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2024-11-02 14:44:54 +0100

    Added return type declarations.

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,9 +1,9 @@
 from controller.auth_controller import TokenData
-from model.user_model import get_user_by_id
+from model.user_model import UserModel, get_user_by_id
 
 
 class UserController:
     @staticmethod
-    def me(token_data: TokenData):
+    def me(token_data: TokenData) -> UserModel:
         user = get_user_by_id(token_data.user_id)
         return user
--- route/user_router.py
+++ route/user_router.py
@@ -4,11 +4,12 @@ from fastapi import Depends, APIRouter
 
 from controller.user_controller import UserController
 from controller.auth_controller import TokenData, parse_token
+from model.user_model import UserModel
 
 
 user_router = APIRouter()
 
 
 @user_router.get('/me')
-def me(token_data: Annotated[TokenData, Depends(parse_token)]):
+def me(token_data: Annotated[TokenData, Depends(parse_token)]) -> UserModel:
     return UserController.me(token_data)
