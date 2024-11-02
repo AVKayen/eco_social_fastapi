6f82b4d33efb2c8c4faeb6ac09491fa9a21ea4a4
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2024-11-02 11:52:56 +0100

    Tagged some fields in user model as Optional. Added create_user function.

--- model/user_model.py
+++ model/user_model.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 from bson import ObjectId
 from db.session import session
 
@@ -7,18 +8,23 @@ from datetime import datetime
 
 class FriendshipRequest(BaseModel):
     user_id: str
-    send_at: datetime
+    send_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
 
 
 class UserModel(BaseModel):
     id: str = Field(..., alias='_id')
     username: str
-    streak: int = 0
-    points: int = 0
-    activities: list[str] = []
-    friends: list[str]
-    incoming_requests: list[FriendshipRequest]
-    outgoing_requests: list[FriendshipRequest]
+    streak: Optional[int] = 0
+    points: Optional[int] = 0
+    activities: Optional[list[str]] = []
+    friends: Optional[list[str]] = []
+    incoming_requests: Optional[list[FriendshipRequest]] = []
+    outgoing_requests: Optional[list[FriendshipRequest]] = []
+
+
+class NewUserModel(BaseModel):
+    username: str
+    password_hash: str
 
 
 def get_user_by_id(_id: str):
@@ -30,3 +36,8 @@ def get_user_by_id(_id: str):
 def get_user_password_by_username(username: str):
     result = session.users_collection().find_one({'username': username})
     return result['password_hash']
+
+
+def create_user(user: NewUserModel):
+    inserted_id = session.users_collection().insert_one(dict(user)).inserted_id
+    return inserted_id
