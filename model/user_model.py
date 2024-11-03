ab7ada6d300ea13798bd36d862dff2b9cc694626
Mateusz Bober <mateusz.bober@gmail.com>
2024-11-03 13:49:29 +0100

    changed friend structure

--- model/user_model.py
+++ model/user_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from typing import Any
 from bson import ObjectId
 from db.session import session
@@ -7,7 +7,7 @@ from pydantic import Field
 
 
 class FriendshipRequest(BaseModel):
-    user_id: str
+    user_id: ObjectId
     sent_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 
@@ -95,3 +95,103 @@ def is_user_friend(user1_id: str, user2_id: str) -> bool:
         'friends': {'$elemMatch': {'user_id': ObjectId(user2_id)}}
     })
     return bool(result)
+
+
+def get_friends(_id: str) -> list[ObjectId] | None:
+    result = session.users_collection().find({
+        "_id": ObjectId(_id),
+        "friends" : 1
+    })[0]
+    if result is None:
+        return None
+    return result
+
+
+def get_incoming_requests(_id: str) -> list[FriendshipRequest] | None:
+    result = session.users_collection().find({
+        "_id": ObjectId(_id),
+        "incoming_requests" : 1
+    })[0]
+    if result is None:
+        return None
+    return result
+
+
+def get_outgoing_requests(_id: str) -> list[FriendshipRequest] | None:
+    result = session.users_collection().find({
+        "_id": ObjectId(_id),
+        "outgoing_requests" : 1
+    })[0]
+    if result is None:
+        return None
+    return result
+
+
+def send_request(my_id: ObjectId, friend_id: ObjectId) -> FriendshipRequest:
+    request_to_friend = FriendshipRequest(friend_id=friend_id)
+    request_from_me = FriendshipRequest(friend_id=my_id)
+    query = {"_id": my_id}  #add request to my outgoing_requests
+    update = {"$push": {"outgoing_requests": dict(request_to_friend)}}
+    session.users_collection().update_one(query, update)
+
+    query = {"_id": friend_id}  #add request to friend's incoming_requests
+    update = {"$push": {"incoming_requests": dict(request_from_me)}}
+    session.users_collection().update_one(query, update)
+
+    return request_to_friend
+
+
+def cancel_request(my_id: ObjectId, friend_id: ObjectId) -> FriendshipRequest:
+    request_to_friend = FriendshipRequest(friend_id=friend_id)
+    request_from_me = FriendshipRequest(friend_id=my_id)
+    query = {"_id": my_id}  #delete request instance for me
+    update = {"$pull": {"outgoing_requests": {"$in": dict(request_to_friend)}}}
+    session.users_collection().update_one(query, update)
+
+    query = {"_id": friend_id}  #delete request instance for friend
+    update = {"$pull": {"incoming_requests": {"$in": dict(request_from_me)}}}
+    session.users_collection().update_one(query, update)
+
+    return request_to_friend
+
+
+def disapprove_request(my_id: ObjectId, friend_id: ObjectId) -> FriendshipRequest:
+    request_to_friend = FriendshipRequest(friend_id=friend_id)
+    request_from_me = FriendshipRequest(friend_id=my_id)
+    query = {"_id": my_id}  #delete request instance for me
+    update = {"$pull": {"incoming_requests": {"$in": dict(request_to_friend)}}}
+    session.users_collection().update_one(query, update)
+
+    query = {"_id": friend_id}  #delete request instance for friend
+    update = {"$pull": {"outgoing_requests": {"$in": dict(request_from_me)}}}
+    session.users_collection().update_one(query, update)
+
+    return request_to_friend
+
+
+def approve_request(my_id: ObjectId, friend_id: ObjectId) -> FriendshipRequest:
+    request_to_friend = FriendshipRequest(friend_id=friend_id)
+    request_from_me = FriendshipRequest(friend_id=my_id)
+    query = {"_id": my_id}  #add friend to my instance
+    update = {"$push": {"friends": dict(request_to_friend)}}
+    session.users_collection().update_one(query, update)
+
+    query = {"_id": friend_id}  #add me to friend's instance
+    update = {"$push": {"friends": request_from_me}}
+    session.users_collection().update_one(query, update)
+
+    disapprove_request(my_id, friend_id)  #remove requests
+
+    return request_to_friend
+
+
+def delete_friend(my_id: ObjectId, friend_id: ObjectId) -> ObjectId:
+    query = {"_id": my_id}  #delete friend instance for me
+    update = {"$pull": {"friends": {"$in": [friend_id]}}}
+    session.users_collection().update_one(query, update)
+
+    query = {"_id": friend_id}  #delete request instance for friend
+    update = {"$pull": {"friends": {"$in": [my_id]}}}
+    session.users_collection().update_one(query, update)
+
+    return friend_id
