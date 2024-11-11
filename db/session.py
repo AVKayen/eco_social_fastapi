2c35db5272039eb57dd008c9c4ba389002300f8b
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2024-11-11 14:38:58 +0100

    Updated the Session constructor so that it verifies if the client is actually connected to the db.

--- db/session.py
+++ db/session.py
@@ -6,39 +6,14 @@ from typing import Any
 from config.settings import settings
 
 
-# COLLECTIONS = [
-#     {
-#         'name': 'users',
-#         'validator': {
-#             '$jsonSchema': {
-#                 'bsonType': 'object',
-#                 'required': ['username', 'password_hash'],
-#                 'properties': {
-#                     'username': {
-#                         'bsonType': 'string',
-#                     },
-#                     'password_hash': {
-#                         'bsonType': 'string',
-#                     }
-#                 }
-#             }
-#         }
-#     }
-# ]
-
-
 class Session:
     def __init__(self, conn_string: str):
         self._client: MongoClient[dict[str, Any]] = MongoClient(conn_string)
         self._db = self._client.get_database('eco_social')
+
+        # Check if the connection is valid
+        self._client.server_info()
         print('Connected to the MongoDB')
-
-        # for collection in self._db.list_collections():
-        #     print(collection)
-
-    # def create_collection(self, name: str, validator: dict):
-    #     self._db.create_collection(name)
-    #     self._db.command('collMod', name, validator=validator)
 
     def db(self) -> Database:
         return self._db
